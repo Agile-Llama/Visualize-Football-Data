766f5835531104d3b53a4480ded07281f27df98a
Chase Hagger <agilellama@gmail.com>
2020-07-17 15:30:01 +1200

    need to serislize this as its going to take an age

--- defensive-visualisation.py
+++ defensive-visualisation.py
@@ -2,8 +2,10 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 from tqdm.auto import tqdm
-
-from data_functions import compute_defensive_actions, minutes_played
+import matplotlib.colors
+import matplotlib.patches as patches
+import matplotlib.cm as cm
+from data_functions import compute_defensive_actions, minutes_played, draw_field
 
 messi_name = 'Lionel Andrés Messi Cuccittini'
 
@@ -17,11 +19,23 @@ messi_csv_file = 'messi_events.csv'
 
 # Defensive map needs the data for the entire season for a team then compare the game with highest/lowest xp to the average.
 
-def calculate_defensive_actions_season(season_id = 21, highest = True):
+def calculate_defensive_actions_season(season_id = 21, highest = False):
+
+    """
+        Function which will get the defensive stats for a particular season accross all of the league. Then create a map which will show if a 
+        particular game has more or less defensive pressure than the seasons mean.
+
+        Args:
+            season_id (int): season of which to get the defensive stats for.
+            highest (bool): Whtether to use the highest or lowest xg of Messi to compare against.
+        
+        Returns:
+
+    """
     df_messi = pd.read_csv(messi_csv_file, index_col=0, low_memory=False)
 
     # A list of all the match ids of a given season.
-    all_match_ids = df_messi[df_messi['season.season_id'] == season_id]['match_id'].values
+    all_match_ids = df_messi[df_messi['season_id'] == season_id]['match_id'].values
 
     x_min_cut = range(0, 120, 20)
     x_max_cut = range(20, 121, 20)
@@ -60,13 +74,47 @@ def calculate_defensive_actions_season(season_id = 21, highest = True):
 
     # Can get either the highest or lowest expected goal match id.
     if highest:
-        match_id = get_highest_xp()
+        match_id = get_highest_xG(df_messi, all_match_ids)
     else:
-        match_id = get_lowest_xg()
+        match_id = get_lowest_xG(df_messi, all_match_ids)
+    
+    df_messi[df_messi['match_id'] == match_id][['home_score', 'away_score', 'home_team.home_team_name', 'away_team.away_team_name']]
+
+    #--
+    
+    fig, ax = plt.subplots()
+    fig.set_size_inches(14, 8)
+    draw_field(ax)
+
+    minima = -20
+    maxima = 20
+
+    norm = matplotlib.colors.Normalize(vmin=minima, vmax=maxima, clip=True)
+    mapper = cm.ScalarMappable(norm=norm, cmap= 'coolwarm')
+
+    plt.colorbar(mapper, shrink=0.7, label='Distance from average')
+
+    # Visualization of defensive actions of Espanyol
+    for index, row in season_dataframe[season_dataframe['match_id'] == match_id].iterrows():
+        mean_def = season_dataframe_mean[(season_dataframe_mean['x_min'] == row['x_min']) & (season_dataframe_mean['x_max'] == row['x_max']) &
+                (season_dataframe_mean['y_min'] == row['y_min']) & (season_dataframe_mean['y_max'] == row['y_max']) ]['def_opponent'].values[0]
+
+        # Create a Rectangle patch
+        rect = patches.Rectangle((row['x_min'], row['y_min']), row['x_max'] - row['x_min'], row['y_max'] - row['y_min'], linewidth=1,
+            edgecolor='none', facecolor=mapper.to_rgba(row['def_opponent']-mean_def),)
+
+        # Add the patch to the Axes
+        ax.add_patch(rect)
+
+    ax.add_patch(patches.Ellipse((40, 10), width=70, height=24, edgecolor='red', fill=False, linewidth=4 ))
+
+    fig.savefig('espanyol_defensive_map.png')
+
+
 
 
 # Return the match id with the lowest xG
-def get_lowest_xg(df_messi, all_match_ids):
+def get_lowest_xG(df_messi, all_match_ids):
     xg_0910 = df_messi[(df_messi['match_id'].isin(all_match_ids)) & (df_messi['player.name'] == messi_name) ].groupby('match_id')['shot.statsbomb_xg'].sum()
 
     xg_0910_per90 = pd.Series(data=[90 * xg_0910[x] / minutes_played(df_messi, x, messi_name, 'Barcelona' ) for x in xg_0910.index],index=xg_0910.index)
@@ -77,7 +125,7 @@ def get_lowest_xg(df_messi, all_match_ids):
     return xg_0910_per90.index[index_min_xg]
 
 # Return the match id with the highest xG
-def get_highest_xg(df_messi, all_match_ids):
+def get_highest_xG(df_messi, all_match_ids):
     xg_0910 = df_messi[(df_messi['match_id'].isin(all_match_ids)) & (df_messi['player.name'] == messi_name) ].groupby('match_id')['shot.statsbomb_xg'].sum()
 
     xg_0910_per90 = pd.Series(data=[90 * xg_0910[x] / minutes_played(df_messi, x, messi_name, 'Barcelona' ) for x in xg_0910.index],index=xg_0910.index)
@@ -86,9 +134,6 @@ def get_highest_xg(df_messi, all_match_ids):
 
     # The game with the highest xG
     return xg_0910_per90.index[index_max_xg]
-
-
-
 
 
 if __name__ == '__main__':
